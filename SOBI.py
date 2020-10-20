6953c8f593b0915302dc6b26cf052f4f861d80f1
edouardpineau <pineau.edouard@gmail.com>
2020-10-20 11:00:23 +0200

    Update SOBI.py

--- SOBI.py
+++ SOBI.py
@@ -1,14 +1,64 @@
 import numpy as np
+import pandas as pd
 import itertools
 
 def center(X, mean=None):
+    
+    """
+    Function to center the data using empirical mean to have each variable 
+    with zero mean.
+    
+    Attributes:
+        * X: data to center
+        * mean: if the true mean is known, can be used (default=None)
+    
+    Returns: 
+        * Centered data
+    """
     
     if mean is None:
         return X - X.mean(axis=1, keepdims=True)
     else:
         return X - mean
+        
+        
+def whitening(X):
+    
+    """
+    Function that withens the data.
+    
+    Attributes:
+        * X: time series data
+        
+    Returns:
+        * Whitened data
+        * Singular vectors
+        * singular values
+    
+    """
+    
+    X = center(X)
+    U, d, _ = np.linalg.svd(X, full_matrices=False)
+    U_d = (U / d).T
+    X_whiten = np.dot(U_d, X) * np.sqrt(X.shape[1])
+    
+    return X_whiten, U, d
+    
     
 def time_lagged_autocov(X, lags):
+    
+    """
+    Computes the auto-covariance tensor, containing all lagged-autocovariance 
+    with lag from 0 (covariance) to lags
+    
+    Attributes:
+        * X: time series data (dimension: variables x time)
+        * lags: number of lags to consider
+        
+    Returns:
+        * Autocovariance tensor
+        
+    """
     
     lags = lags + 1
     n, l = X.shape
@@ -20,33 +70,47 @@ def time_lagged_autocov(X, lags):
     for k in range(lags):
         Xk = center(X[:, k:k+L])
         R[k] = (1.0/L)*(X0.dot(Xk.T))
-        
         R[k] = 0.5*(R[k] + R[k].T)
     
     return R
-
-def whitening(X, C=None):
     
-    if C is None:
-        C = X.shape[0]
-        
-    X = center(X)
-    U, d, _ = np.linalg.svd(X, full_matrices=False)
-    U_d = (U / d).T[:C]
-    X_whiten = np.dot(U_d, X) * np.sqrt(X.shape[1]) # np.dot(E, np.dot(D_inv, np.dot(E.T, X)))
-    
-    return X_whiten, U, d
     
 def off_frobenius(M):
+    
+    """
+    Computes the square Frobenius norm of the matrix M-diag(M)
+    
+    Attributes:
+        * M: square matrix
+        
+    Returns:
+        * Off-diagonal Frobenius norm
+    
+    
+    """
     
     return (np.linalg.norm(np.tril(M, k=-1), ord='fro')**2 + np.linalg.norm(np.triu(M, k=1), ord='fro')**2)
 
 def rotation(M):
     
+    """
+    This function infers Jacobi rotation matrix R used in the joint 
+    diagonalization of a set of matrices
+    
+    See: https://en.wikipedia.org/wiki/Jacobi_rotation
+    
+    Attributes:
+        * M: matrix to be rotated
+        
+    Returns:
+        * Rotation matrix
+    
+    
+    """
+    
     h = np.array([M[:, 0, 0] - M[:, 1, 1], 
                   M[:, 1, 0] + M[:, 0, 1], 
                   1j*(M[:, 1, 0] - M[:, 0, 1])]).T
-    # G = np.stack([np.expand_dims(h_, axis=1).dot(np.expand_dims(h_, axis=0)) for h_ in h]).sum(0)
     G = np.real(h.T.dot(h))
     [eigvals,v] = np.linalg.eigh(G)
     [x, y, z] = np.sign(v[0, -1])*v[:,-1]
@@ -60,6 +124,22 @@ def rotation(M):
     return R
 
 def joint_diagonalization(C, V=None, eps=1e-3, max_iter=1000, verbose=-1):
+    
+    """
+    Joint diagonalization of a set of matrices C
+    
+    Attributes:
+        * C: set of symmetric matrices
+        * V:  a priori eigan-vectors of the matrices C (default=None)
+        * eps: tolerance for stopping criteria (default=1e-3)
+        * max_iter: maximum number of iterations taken for the solvers to 
+          converge (default=1000)
+    
+    Returns:
+        * V: a posteriori eigen-vectors of the matrices C
+        * C: diagonalized matrices
+        
+    """
     
     d = C.shape[1]
     list_pairs = list(itertools.combinations(range(d), 2))
@@ -93,14 +173,3 @@ def joint_diagonalization(C, V=None, eps=1e-3, max_iter=1000, verbose=-1):
         O_cs = O_cs_new
         
     return V, C
-
-def ICA(X, lag=1, eps=1e-3, max_iter=1000):
-    
-    X_white, U, d = whitening(X)
-    C = time_lagged_autocov(X_white, lag)
-    C = C + 1J*np.zeros_like(C)
-    V, C = joint_diagonalization(C, eps=eps, max_iter=max_iter)
-    W = (V.T).dot((U / d).T)
-    S = np.real(W.dot(X))
-    
-    return S
